cb60e66adbda542c02f161c783360914bbc3d7dc
liushengzhong <sl29@illinois.edu>
2018-02-20 02:08:06 -0600

    Add functionality in AnalysisLib: extract stats probability from dataset.

--- src/AnalysisLib/AnalysisLib.py
+++ src/AnalysisLib/AnalysisLib.py
@@ -1,14 +1,71 @@
 from Dependency.ObjectPreference import ObjectPreference
 from Dependency.HourlyActionRate import HourlyActionRate
+import numpy as np
 
 class AnalysisLib:
-    agentIds = [0,1,2,3,4]
-    objectIds = [0,1,2]
-    probs = [.2,.3,.5]
-    testDist = [0,0,0,0,0,0,0,0,0,0.1,0.2,0.2,0.1,0.1,0.1,0.1,0.1,0.0,0.0,0,0,0,0,0]
+    agentIds = []
+    objectIds = []
+    agentObjectPreference = {}
+    agentHourlyActionRate = {}
+    agentActionCount = {}
+    generalObjectPreference = {}
+    generalHourlyActionRate = np.array([0.0 for i in range(24)])
     
     def __init__(self):
-        pass
+        with open("../../data/push_short.txt") as file:
+            for line in file:
+                if not line:
+                    break
+                else:
+                    #Event format: (eventTime,objectId,actorId)
+                    event = line.split(",")
+                    eventTime = int(event[0])
+                    hour = (eventTime/3600)%24
+                    objectId = int(event[1])
+                    agentId = int(event[2])
+
+                    #Update the agentsIds and his records
+                    if agentId not in AnalysisLib.agentIds: #This is a new user.
+                        AnalysisLib.agentIds.append(agentId)
+                        AnalysisLib.agentActionCount[agentId] = 1
+
+                        #Update the agentHourlyActionRate
+                        hourlyActions = np.array([0.0 for i in range(24)])
+                        hourlyActions[hour] += 1
+                        AnalysisLib.agentHourlyActionRate[agentId] = hourlyActions
+
+                        #Update the agentObjectPreference
+                        objectPreference = {objectId: 1.0}
+                        AnalysisLib.agentObjectPreference[agentId] = objectPreference
+                    else: #Not a new user.
+                        AnalysisLib.agentActionCount[agentId] += 1
+                        AnalysisLib.agentHourlyActionRate[agentId][hour] += 1
+                        if objectId not in AnalysisLib.agentObjectPreference[agentId]: #Did not touch this object before
+                            AnalysisLib.agentObjectPreference[agentId][objectId] = 1.0
+                        else:
+                            AnalysisLib.agentObjectPreference[agentId][objectId] += 1
+                    #Update the objectIds
+                    if objectId not in AnalysisLib.objectIds:
+                        AnalysisLib.objectIds.append(objectId)
+
+                    #Update the generalObjectPreference and generalHourlyActionRate
+                    if objectId not in AnalysisLib.generalObjectPreference:
+                        AnalysisLib.generalObjectPreference[objectId] = 1.0
+                    else:
+                        AnalysisLib.generalObjectPreference[objectId] += 1
+                    AnalysisLib.generalHourlyActionRate[hour] += 1
+
+        #Update the agentHourActionRate and agentObjectPreference
+        for agentId in AnalysisLib.agentIds:
+            AnalysisLib.agentHourlyActionRate[agentId] /= AnalysisLib.agentActionCount[agentId]
+            for objectId in AnalysisLib.agentObjectPreference[agentId]:
+                AnalysisLib.agentObjectPreference[agentId][objectId] /= AnalysisLib.agentActionCount[agentId]
+
+        #Update the generalObjectPreference and generalHourlyActionRate
+        totalActions = sum(AnalysisLib.agentActionCount.values())
+        AnalysisLib.generalHourlyActionRate /= totalActions
+        for objectId in AnalysisLib.generalObjectPreference:
+            AnalysisLib.generalObjectPreference[objectId] /= totalActions
 
     
     @staticmethod
@@ -31,9 +88,20 @@ class AnalysisLib:
         '''
         :return: a list of HourlyActionRate instances, one for each actionType 
         '''
-        
-        pullRequestAction = HourlyActionRate(agentId, "GITHUB_PULL_REQUEST", AnalysisLib.testDist) 
-        pushAction = HourlyActionRate(agentId, "GITHUB_PUSH", AnalysisLib.testDist) 
+        if agentId in AnalysisLib.agentIds:
+            pullRequestAction = HourlyActionRate(agentId,
+                                                 "GITHUB_PULL_REQUEST",
+                                                 AnalysisLib.agentHourlyActionRate[agentId])
+            pushAction = HourlyActionRate(agentId,
+                                          "GITHUB_PUSH",
+                                          AnalysisLib.agentHourlyActionRate[agentId])
+        else: #This is a new user, no record.
+            pullRequestAction = HourlyActionRate(agentId,
+                                                 "GITHUB_PULL_REQUEST",
+                                                 AnalysisLib.generalHourlyActionRate)
+            pushAction = HourlyActionRate(agentId,
+                                          "GITHUB_PUSH",
+                                          AnalysisLib.generalHourlyActionRate)
 
         return [pushAction, pullRequestAction]
         
@@ -43,11 +111,21 @@ class AnalysisLib:
         '''
         :return: an ObjectPreference instance
         '''
-        objectPreference = ObjectPreference(agentId, AnalysisLib.objectIds, AnalysisLib.probs)
+        if agentId in AnalysisLib.agentIds:
+            objectPreference = ObjectPreference(agentId,
+                                                AnalysisLib.agentObjectPreference[agentId].keys(),
+                                                AnalysisLib.agentObjectPreference[agentId].values())
+        else: #This is a new user, no record.
+            objectPreference = ObjectPreference(agentId,
+                                                AnalysisLib.generalObjectPreference.keys(),
+                                                AnalysisLib.generalObjectPreference.values())
         return objectPreference
     
         
     @staticmethod
     def getAgentDependentActions(agentID):
         return None
-    
+
+
+if __name__ == '__main__':
+    analysislib = AnalysisLib()

8b3a30fe0da42d9783c11142fbc68c4a772d6610
haoyifan <haoyifan@umich.edu>
2018-02-26 09:37:34 -0600

    Add hook script and add into setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# setup environment variable
 echo "export PYTHONPATH=$PYTHONPATH:$(pwd)/src" >> ~/.profile
 echo "export SOCIALSIMPATH=$(pwd)" >> ~/.profile
 
+# setup pre-commit hook
+cp ./script/hook/pre-commit ./.git/hooks
